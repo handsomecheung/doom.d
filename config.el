d4eca4b991bfc09931ae86274fd556b578e00d86
hc <handsomecheung@gmail.com>
2022-02-13 22:34:33 +0800

    theme doom-dark+ for terminal mode

--- config.el
+++ config.el
@@ -25,7 +25,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(if (not (display-graphic-p))
+      (load-theme 'doom-dark+ t)
+  (load-theme 'doom-one t))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
